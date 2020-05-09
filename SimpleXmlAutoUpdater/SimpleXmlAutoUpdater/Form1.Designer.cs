2c29f67b2aebf10b08bfe72ce48c9be1edf914c3
rakion99 <_rakion99@outlook.com>
2020-05-09 14:01:22 -0500

    push

---
+++ SimpleXmlAutoUpdater/SimpleXmlAutoUpdater/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SimpleXmlAutoUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckforupdatesBTN = new System.Windows.Forms.Button();
+            this.CurrentSha384TextBox = new System.Windows.Forms.TextBox();
+            this.CurrentSha384Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CheckforupdatesBTN
+            // 
+            this.CheckforupdatesBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckforupdatesBTN.Location = new System.Drawing.Point(279, 95);
+            this.CheckforupdatesBTN.Name = "CheckforupdatesBTN";
+            this.CheckforupdatesBTN.Size = new System.Drawing.Size(143, 39);
+            this.CheckforupdatesBTN.TabIndex = 0;
+            this.CheckforupdatesBTN.Text = "Check for Updates";
+            this.CheckforupdatesBTN.UseVisualStyleBackColor = true;
+            this.CheckforupdatesBTN.Click += new System.EventHandler(this.CheckforupdatesBTN_Click);
+            // 
+            // CurrentSha384TextBox
+            // 
+            this.CurrentSha384TextBox.Location = new System.Drawing.Point(28, 49);
+            this.CurrentSha384TextBox.Name = "CurrentSha384TextBox";
+            this.CurrentSha384TextBox.Size = new System.Drawing.Size(728, 20);
+            this.CurrentSha384TextBox.TabIndex = 3;
+            // 
+            // CurrentSha384Label
+            // 
+            this.CurrentSha384Label.AutoSize = true;
+            this.CurrentSha384Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentSha384Label.Location = new System.Drawing.Point(275, 9);
+            this.CurrentSha384Label.Name = "CurrentSha384Label";
+            this.CurrentSha384Label.Size = new System.Drawing.Size(141, 20);
+            this.CurrentSha384Label.TabIndex = 4;
+            this.CurrentSha384Label.Text = "Current Sha384:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 152);
+            this.Controls.Add(this.CurrentSha384Label);
+            this.Controls.Add(this.CurrentSha384TextBox);
+            this.Controls.Add(this.CheckforupdatesBTN);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SimpleXmlAutoUpdater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckforupdatesBTN;
+        private System.Windows.Forms.TextBox CurrentSha384TextBox;
+        private System.Windows.Forms.Label CurrentSha384Label;
+    }
+}
+
---
+++ SimpleXmlAutoUpdater/SimpleXmlAutoUpdater/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SimpleXmlAutoUpdater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CurrentSha384TextBox.Text = GetSHA384("./SimpleXmlAutoUpdater.exe.config");
+        }
+
+        private static readonly XmlDocument XmlUpdateFileDoc = new XmlDocument();
+        private static string XmlGetSingleNode(string nodelocation) => XmlUpdateFileDoc.DocumentElement.SelectSingleNode(nodelocation).InnerText;
+
+        private void CheckForUpdates()
+        {
+            ServicePointManager.Expect100Continue = true;//win7 fix
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;//win7 fix
+            using (WebClient client = new WebClient())
+            {
+                client.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
+                XmlUpdateFileDoc.LoadXml(client.DownloadString("https://pastebin.com/raw/8HpBBdPd"));//using pastebin as github kinda broken for webclient
+            }
+
+            if (XmlGetSingleNode("/App/Sha384") != CurrentSha384TextBox.Text)
+            {
+                MessageBox.Show("Update found for App", "Update Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //simple code to download update
+                /*using (WebClient WebC = new WebClient())
+                {
+                    var data = WebC.DownloadData(XmlGetSingleNode("/App/DownloadUrl"));
+                    File.WriteAllBytes(@".\SimpleXmlAutoUpdater.exe", data);
+                }*/
+            }
+            else
+                MessageBox.Show("No Updates Found");
+        }
+
+        private static string GetSHA384(string filepath)
+        {
+            FileStream filestream = new FileStream(filepath, FileMode.Open)
+            {
+                Position = 0
+            };
+
+            return BitConverter.ToString(System.Security.Cryptography.SHA384.Create().ComputeHash(filestream)).Replace("-", string.Empty);
+        }
+
+        private void CheckforupdatesBTN_Click(object sender, EventArgs e) => CheckForUpdates();
+    }
+}
